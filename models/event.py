f2f34295c9cdcef5226793f000b8887341f0b552
Ale <>
2022-12-22 09:29:00 +0100

    desarrollo del modelo event

--- models/event.py
+++ models/event.py
@@ -3,14 +3,13 @@
 from odoo import models, fields, api
 
 class Event(models.Model):
-    name = 'grupo3c.event'
+    _name = 'grupo3c.event'
 
-    name = fields.Char()
-    description = fields.Text()
-    #value = fields.Integer()
-    #value2 = fields.Float(compute="_value_pc", store=True)
-    #description = fields.Text()
-
-    #@api.depends('value')
-    #def _value_pc(self):
-    #self.value2 = float(self.value) / 100
+    eventId = fields.Integer(string = "Event Id", requitred = True)
+    eventType = fields.Char(string = "Event Type", requitred = True)
+    description = fields.Text(string = "Description", requitred = True)
+    numPart = fields.Integer(string= "Number of Participants", requitred = True)
+    date = fields.Date(string = "Date", requitred = True)
+    admin_id = fields.Many2one('admin',string="Organizer", requitred = True)
+    customer_id = fields.Many2many('customer', string = "Customers", requitred = True)
+    place = fields.Many2one('place', string = "Place", requitred = True)
