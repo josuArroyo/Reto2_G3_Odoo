8b65bd92be9305d2a37ad0648612d88e70d097bd
Diego <Conkergox98@gmail.com>
2023-02-02 12:05:31 +0100

    Validaciones de objetivo realizadas

--- models/objective.py
+++ models/objective.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 class Objective(models.Model):
     _name = 'grupo3c.objective'
@@ -13,3 +13,18 @@ class Objective(models.Model):
     training_id = fields.One2many("grupo3c.training", "objectiveId", string="Training_code")
     admin = fields.Many2one("res.users", string="Trainer_identification")
     clients = fields.Many2many("res.users", string="Client_identification")
+    
+    @api.onchange('description')
+    def _onchange_description(self):
+        if len(str(self.description)) > 100:
+            return{
+        'warning' : {
+            'title':"something bad happened",
+            'message':"you have reached the maximum characters allowed"
+            }
+        }
+    @api.constrains('paramValue')
+    def _validate_paramvalue(self):
+        for objective in self:
+             if len(str(self.paramValue)) > 15:
+                raise exceptions.ValidationError("you have reached the maximum characters allowed")
